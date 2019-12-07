baf65b38c558e4111fad8d5189e3e6c1d736878f
gozdeylmz95 <gozdeylmz95@gmail.com>
2019-12-07 16:05:40 +0300

    Updates

---
+++ precache-manifest.e2cdd86340579c414283375562c8bdcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ae22d7af5a54d183abbe479907b65b",
+    "url": "/react-pokemon/index.html"
+  },
+  {
+    "revision": "541e6ed9e53d47fe886e",
+    "url": "/react-pokemon/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a950ee6f0376d73101bf",
+    "url": "/react-pokemon/static/js/2.1684ca6a.chunk.js"
+  },
+  {
+    "revision": "541e6ed9e53d47fe886e",
+    "url": "/react-pokemon/static/js/main.9d615c65.chunk.js"
+  },
+  {
+    "revision": "32a9a81dc117a97c9134",
+    "url": "/react-pokemon/static/js/runtime-main.f0aa07a8.js"
+  }
+]);
